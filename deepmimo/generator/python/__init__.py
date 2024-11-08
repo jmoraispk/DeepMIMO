a3a039c7379ecd1f9a494379c9c6ff824a9786ef
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2024-11-07 22:15:21 -0700

    del needless file

--- deepmimo/generator/python/__init__.py
+++
@@ -1,4 +0,0 @@
-from .generator import generate_data
-from .params import default_params
-from .construct_deepmimo import ant_indices, array_response
-# from .sionna_adapter import DeepMIMOSionnaAdapter
