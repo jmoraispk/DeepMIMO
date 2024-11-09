e13c89916fb26343cabbcd497dfb1b3933138cd0
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2024-11-08 21:49:06 -0700

    wireless insite converter working!

--- deepmimo/converter/wireless_insite/insite_converter.py
+++ deepmimo/converter/wireless_insite/insite_converter.py
@@ -4,42 +4,36 @@ Created on Thu Jan 19 17:55:17 2023
 
 @author: demir
 """
+import os
+import numpy as np
+import scipy.io
 
 from .ChannelDataLoader import WIChannelConverter
 from .ChannelDataFormatter import DeepMIMODataFormatter
 from .scenario_utils import ScenarioParameters
-import os
 
 
 def insite_rt_converter(p2m_folder):
 
-    #%% Parameters to be given for the scenario
-    # p2m_folder = r'C:\Users\Umt\Desktop\RIS_Indoor_v2\p2m'
-    intermediate_folder = os.path.join(os.path.dirname(p2m_folder), 'intermediate_filesv2')
-    output_folder = os.path.join(os.path.dirname(p2m_folder), 'mat_filesv2')
+    # Parameters to be given for the scenario
+    intermediate_folder = os.path.join(os.path.dirname(p2m_folder), 'intermediate_files')
+    output_folder = os.path.join(os.path.dirname(p2m_folder), 'mat_files')
     
     os.makedirs(intermediate_folder, exist_ok=True)
     os.makedirs(output_folder, exist_ok=True)
 
-    #%% Convert P2M files to mat format
-    # WIChannelConverter(p2m_folder, intermediate_folder)
+    # Convert P2M files to mat format
+    WIChannelConverter(p2m_folder, intermediate_folder)
 
-    #%%
     dm = DeepMIMODataFormatter(intermediate_folder, output_folder, max_channels=100000,
-                               TX_order=[1], RX_order=[1])
+                               TX_order=[1], RX_order=[4])
 
-    #%%
-    import numpy as np
-    import scipy.io
-
-    # output_folder = r'C:\Users\Umt\Desktop\dynamic_scenario\dyn3'
     data_dict = {
                 'version': 2,
                 'carrier_freq': 28e9,
                 'transmit_power': 0.0, #dB from the scenario
-                'user_grids': np.array([[1, 171, 151] # Start row - end row - num users - Num users must be larger than the maximum number of dynamic receivers
-                                        ], 
-                                        dtype=float),
+                # Start row - end row - num users - Num users must be larger than the maximum number of dynamic receivers
+                'user_grids': np.array([[1, 411, 321]], dtype=float),
                 'num_BS': len(dm.TX_order),
                 'dual_polar_available': 0,
                 'doppler_available': 0
@@ -47,3 +41,4 @@ def insite_rt_converter(p2m_folder):
                 }
         
     scipy.io.savemat(os.path.join(output_folder, 'params.mat'), data_dict)
+
--- testing.py
+++ testing.py
@@ -12,6 +12,8 @@ path_to_p2m_folder = r'C:\Users\jmora\Documents\GitHub\DeepMIMO\P2Ms\ASU_campus_
 
 dm.create_scenario(path_to_p2m_folder)
 
+
+
 #%%
 params = dm.Parameters('city_2_chicago')#asu_campus')
 
