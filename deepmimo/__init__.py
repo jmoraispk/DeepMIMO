169fd924b9a6de1fdd24b6bfc977924e91c16e37
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2024-11-30 08:28:19 -0700

    rename help to info (no collision with built-in)

--- deepmimo/__init__.py
+++ deepmimo/__init__.py
@@ -2,5 +2,6 @@
 from .generator.python.generator import generate_data as generate
 from .generator.python.params import Parameters
 from .converter.converter import create_scenario
+from .info import info
 
-__all__ = ['generate', 'create_scenario']
+__all__ = ['generate', 'create_scenario', 'info']
---
+++ deepmimo/info.py
@@ -0,0 +1,26 @@
+
+import .consts as c
+def info(s):
+    if type(s) != str:
+        help(s)
+    elif s == 'materials':
+        website = 'deepmimo.net' # put more accurate website
+        print('Materials are unique. If 2 names appear, they must have different, '
+              'properties. ')
+        print('For more info, inspect the raytracing source available '
+              f'in {website}. ') # JTODO: offer option to dload RT source from link
+        pass
+    help_messages = {
+        c.CHS_PARAM_NAME: '...',
+        c.AOA_PARAM_NAME: '...',
+        c.AOD_PARAM_NAME: '...',
+        c.TOA_PARAM_NAME: '...',
+        c.PWR_PARAM_NAME: '...',
+        c.PHASE_PARAM_NAME: '...',
+        c.RX_LOC_PARAM_NAME: '...',
+        c.TX_LOC_PARAM_NAME: '...',
+        c.INTERACTIONS_PARAM_NAME: '...', 
+        c.INTERACTIONS_LOC_PARAM_NAME: '...',
+        'materials': '...'
+        }
+            
