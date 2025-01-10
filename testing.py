b91f6718d19b4fc6d2e4264774a65321c24eb3a7
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-01-10 04:34:19 -0700

    update latest testing

--- testing.py
+++ testing.py
@@ -6,23 +6,23 @@
 #%% V3 & V4 Conversion
 
 import deepmimo as dm
-path_to_p2m_outputs = r'.\P2Ms\ASU_campus_just_p2m\study_area_asu5'
-# path_to_p2m_outputs = r'.\P2Ms\simple_street_canyon_test\study_rays=0.25_res=2m_3ghz'
+# path_to_p2m_outputs = r'.\P2Ms\asu_campus\study_area_asu5'
+path_to_p2m_outputs = r'.\P2Ms\simple_street_canyon_test\study_rays=0.25_res=2m_3ghz'
 
 scen_name = dm.create_scenario(path_to_p2m_outputs,
-                               copy_source=True, 
                                overwrite=True, 
-                               old=False) # V3 or V4 conversion flag
-#                              old has STATIC PARAMS!
-
-# old: works with both
-# new: works only with simple_street_canyon 
+                               old=True) # V3 or V4 conversion flag
+#                              old has STATIC PARAMS! (asu scenario needs diff grids)
 
 #%% V4 
 import deepmimo as dm
 scen_name = 'simple_street_canyon_test'
-# params.get_params_dict()['user_rows'] = np.arange(91)
-dataset = dm.generate(params)
+
+load_params = {}
+dataset = dm.load(scen_name, **load_params)
+# dataset.gen_channels()
+
+#dataset = dm.generate(params)
 
 #%% V3 Generation
 
@@ -34,6 +34,10 @@ dataset = dm.generate_old(params)
 
 #%% Dream
 
+# Wireless Insite IDXs = [3, 7, 8]
+# DeepMIMO (after conversion) TX/RX Sets: [1, 2, 3]
+# DeepMIMO (after generation) : only individual tx and rx indices
+
 # 0- Conversion is general. Make bindings for a general generation
 #    (by having an old and new version of the code and calling diff funcs)
 # [DONE]
@@ -44,41 +48,33 @@ dataset = dm.generate_old(params)
 # 2- Remove BS-BS specific function (use normal generation)
 # [DONE]
 
-# 3- Add new (multi-txrx) way of generating data
+# 3- Decouple channel generation from the dataset generation
+
+# 4- Add new (multi-txrx) way of generating data
 
 # tx_set = ...
 # Option 1: {1: [0,2,4], 2: [3,4,5,], 3: 'all'}
 # Option 2: [0, 1]
 # Option 3: 'all' (default)
 
-# 4- Decouple channel generation from the dataset generation
+# 5- IMPLEMENT new structure of dataset and generate from new matrices
 
-# 5- Add new smart object: 
+# 6- Add new smart object: 
 #   - dataset.compute_channels()
 #   - dataset.compute_pl() -> unlocks 'pathloss'
 #   - dataset.compute_dists()
 #   - dataset.compute_num_paths()
 #   - dataset.compute_num_interactions() -> unlocks 'num_interactions'
 
-# 5- IMPLEMENT new structure of dataset and generate from new matrices
-
 # ---- (later) ----
+
 # 7- Make it work with ['chs'], ['channels'], etc..
 
 # 8- Simplified building save matrix & plots
 
-#%%
+# 9- RUN BLACK to format all code (consistent)
 
-# Available TX/RX sets
-
-# 1: Tx & Rx    (1 points)
-# 2: Rx      (5551 points) (x antennas)
-
-
-# Wireless Insite IDXs = [3, 7, 8]
-# DeepMIMO (after conversion) TX/RX Sets: [1, 2, 3]
-# DeepMIMO (after generation) : only tx individual indices
-
+# 10- REFACTORING CONVERSION: move MATERIAL and TXRX to separate files
 
 #%% READ Setup
 
