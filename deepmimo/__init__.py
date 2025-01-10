905a2114f8d43f596e1f02a5f3c8e6b020b16b66
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-01-10 04:34:46 -0700

    add v3 old generation to enable comparison

--- deepmimo/__init__.py
+++ deepmimo/__init__.py
@@ -1,4 +1,7 @@
 
+from .generator.python_old.generator import generate_data as generate_old
+from .generator.python_old.params import Parameters as Parameters_old
+
 from .generator.python.generator import generate_data as generate
 from .generator.python.params import Parameters
 from .converter.converter import create_scenario
