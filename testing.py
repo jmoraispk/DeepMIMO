6894666362c91ef98f7efd0bfd0f1f30d743a36b
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-01-17 15:47:34 -0700

    update comments - add new plans

--- testing.py
+++ testing.py
@@ -1,12 +1,13 @@
 #%% V3 & V4 Conversion
 
 import deepmimo as dm
-# path_to_p2m_outputs = r'.\P2Ms\asu_campus\study_area_asu5'
-path_to_p2m_outputs = r'.\P2Ms\simple_street_canyon_test\study_rays=0.25_res=2m_3ghz'
+path_to_p2m_outputs = r'.\P2Ms\asu_campus\study_area_asu5'
+# path_to_p2m_outputs = r'.\P2Ms\simple_street_canyon_test\study_rays=0.25_res=2m_3ghz'
 
-old_params_dict = {'num_bs': 1, 'user_grid': [1, 91, 61],   'freq': 3.5e9}   # simple canyon
-# old_params_dict = {'num_bs': 1, 'user_grid': [1, 411, 321], 'freq': 3.5e9} # asu
-
+if 'asu_campus' in path_to_p2m_outputs:
+    old_params_dict = {'num_bs': 1, 'user_grid': [1, 411, 321], 'freq': 3.5e9} # asu
+else:
+    old_params_dict = {'num_bs': 1, 'user_grid': [1, 91, 61],   'freq': 3.5e9}   # simple canyon
 
 scen_name = dm.create_scenario(path_to_p2m_outputs,
                                overwrite=True, 
@@ -16,11 +17,12 @@ scen_name = dm.create_scenario(path_to_p2m_outputs,
 
 #%% V4 
 import deepmimo as dm
-scen_name = 'simple_street_canyon_test'
+# scen_name = 'simple_street_canyon_test'
+scen_name = 'asu_campus'
 
 # Option 1 - dictionaries per tx/rx set and tx/rx index inside the set)
 tx_sets = {1: [0]}
-rx_sets = {2: 'active'}
+rx_sets = {2: 'all'}
 
 # Option 2 - lists with tx/rx set (assumes all points inside the set)
 # tx_sets = [1]
@@ -33,11 +35,9 @@ load_params = {'tx_sets': tx_sets, 'rx_sets': rx_sets, 'max_paths': 5}
 dataset = dm.load_scenario(scen_name, **load_params)
 dataset['load_params'] = load_params  # c.LOAD_PARAMS_PARAM_NAME
 
-dataset.info() # print available tx-rx information
-
-#%%
-from pprint import pprint
-pprint(dataset)
+# dataset.info() # print available tx-rx information
+# from pprint import pprint
+# pprint(dataset)
 
 #%%
 params = dm.ChannelGenParameters()
@@ -56,14 +56,27 @@ dataset = dm.generate(scen_name)
 #%% V3 Generation
 
 import deepmimo as dm
-scen_name = 'simple_street_canyon_test'
-params = dm.Parameters_old(scen_name)#asu_campus')
+# scen_name = 'simple_street_canyon_test'
+scen_name = 'asu_campus'
+params = dm.Parameters_old(scen_name)
 # params.get_params_dict()['user_rows'] = np.arange(91)
 dataset2 = dm.generate_old(params)
 
 chs2 = dataset2[0]['user']['channel']
 
 # TODOOOO: test PARAMSET_OFDM_LPF = 1! (crashing on the other.)
+
+#%%
+
+import deepmimo as dm
+# scen_name = dm.create_scenario(r'.\P2Ms\asu_campus\study_area_asu5')
+# dataset = dm.generate(scen_name)
+
+# load_params = {'tx_sets': [1], 'rx_sets': [2], 'max_paths': 5}
+# load_params = {'tx_sets': [1], 'rx_sets': {2: 'active'}}
+# load_params = {'tx_sets': [1], 'rx_sets': {2: [1,2,3]}}
+# dataset = dm.load_scenario(scen_name, **load_params)
+# dataset = dm.load_scenario('city_10_austin')
 
 #%% Dream
 
@@ -72,7 +85,7 @@ chs2 = dataset2[0]['user']['channel']
 # DeepMIMO (after conversion) TX/RX Sets: [1, 2, 3]
 # DeepMIMO (after generation) : only individual tx and rx indices
 
-# 6- Make new DeepMIMO work with channel generation 
+# 7- Make new DeepMIMO work with channel generation 
 # [DONE]
 
 # IMPORTANT: There are functions inside compute_channels() that MODIFY the dataset
@@ -98,40 +111,51 @@ chs2 = dataset2[0]['user']['channel']
 #         for now and add it to the right place: generation (not conversion!)
 
 
-# 7 - Add option to load only active users
-
-# ---- (later) ----
+# Essential for release
 
 # 8- Add dataset.info() to dataset (and documentation throughout)
 # dm.info('inter') (or dataset['inter'].info())
 
 # 9- Add new smart object: 
 #   - dataset.compute_los_status()       -> unlocks 'los_status'
-#   - dataset.compute_channels()         -> unlocks 'channels'
+#   - dataset.compute_channels()         -> unlocks 'channels'g
 #   - dataset.compute_pl()               -> unlocks 'pathloss'
 #   - dataset.compute_dists()            -> unlocks 'distance'
 #   - dataset.compute_num_paths()        -> unlocks 'num_paths'
 #   - dataset.compute_num_interactions() -> unlocks 'num_interactions'
-
 
 # 9.1) embed compute_channels in dataset (using the outside function?) -> dataset.gen_channels()
 # 9.2) make gen_channels() that computes all BSs (like dataset.gen_channels() vs dataset[0].gen_channels())
 #       where dataset.gen_channels() just does a for over the len of the dataset and calls gen_channels()
 # 9.3) make generate function that does the loading and channel gen.
 #       dataset = dm.generate(params) -> with both load and ch_gen
+"""
+Plan: possibly 3 objects
+
+1- Macro dataset (containing the information from several transmitters)
+2- Dataset (containing 1tx worth of information)
+3- Matrix (like a numpy matrix, but supports a few more operations
+
+Other notes:
+
+* make dataset.aoa_az and dataset[‘aoa_az’] both possible
+* @property can be used to define functions to be called when doing a.smth
+* @descriptors can be used to define meta properties
+* __help__, __getitem__, __setitem__, __getattr__, can be used to make our dataset object
+"""
+
+
 
 
 # 10- Save building matrix & plot building
+# 12- REFACTORING CONVERSION: move MATERIAL and TXRX to separate files
+# 13- REFACTORING GENERATION: move validation into channel gen script
+
+# Other features:
 
 # 11- RUN RUFF to format all code (consistently - https://github.com/astral-sh/ruff)
 
-# 12- REFACTORING CONVERSION: move MATERIAL and TXRX to separate files
-
-# 13- REFACTORING GENERATION: move validation into channel gen script
-
 # 13- Option to include (or not) interaction locations in dataset (3x more space and time needed)
-
-# 14- Enable loading x paths
 
 # 15- Enable generating the channel only for R/D/... paths
 
@@ -155,9 +179,8 @@ chs2 = dataset2[0]['user']['channel']
 # dataset['aoa'] # N x PATHS x 2 (azimuth / elevation)
 # dataset['aoa'] = # concat...aoa_el / aoa_az
 
-# 20 - Add space requirements to auto downloader
+# 20 - Add space requirements to auto downloader information
 
-####### Obvious TODOs ######
 # 21 - Add dual-polarization AND multi-antenna support
 
 #%% READ Setup
