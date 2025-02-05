9320336d4d52229f76efbb9a7d4052781f87ecba
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-02-04 17:19:19 -0700

    add rt_params, txrx, and paths to sionna converter

--- deepmimo/converter/sionna_rt/sionna_converter.py
+++ deepmimo/converter/sionna_rt/sionna_converter.py
@@ -1,6 +1,32 @@
+"""
+Sionna Ray Tracing Converter Module.
+
+This module provides functionality for converting Sionna Ray Tracing output files
+into the DeepMIMO format. It handles reading and processing ray tracing data including:
+- Path information (angles, delays, powers, interactions, ...)
+- TX/RX locations and parameters 
+- Scene geometry and materials
+"""
+
+import os
+import shutil
 import numpy as np
-from typing import Tuple, List, Dict, Any
+from typing import Dict
 import pickle
+
+# Internal DeepMIMO imports
+from ...txrx import TxRxSet  # For TX/RX set handling
+from ... import consts as c  # Constants and configuration parameters
+from .. import converter_utils as cu  # Shared converter utilities
+
+# Interaction Type Map for Wireless Insite
+INTERACTIONS_MAP = {
+    0:  c.INTERACTION_LOS,           # LoS
+    1:  c.INTERACTION_REFLECTION,    # Reflection
+    2:  c.INTERACTION_DIFFRACTION,   # Diffraction
+    3:  c.INTERACTION_SCATTERING,    # Diffuse Scattering
+    4:  None,  # Sionna RIS is not supported yet
+}
 
 
 def save_to_pickle(obj, filename):
@@ -13,17 +39,264 @@ def load_from_pickle(filename):
     with open(filename, 'rb') as file:
         return pickle.load(file)
 
-def sionna_rt_converter(a):
+def sionna_rt_converter(rt_output_folder: str, scenario_name: str = ''):
     print('converting from sionna RT')
+
+    # Setup scenario name
+    scen_name = os.path.basename(rt_output_folder) 
+    if scenario_name:
+        scen_name = scenario_name
+
+    # Setup output folder
+    output_folder = os.path.join(rt_output_folder, scen_name + '_deepmimo')
+    if os.path.exists(output_folder):
+        shutil.rmtree(output_folder)
+
+    os.makedirs(output_folder)
+
+    read_paths = 0
+    read_vertices = 0
+    read_faces = 0
+    read_objects = 0
+
+    # Read Setup (ray tracing parameters)
+    setup_dict = read_setup(rt_output_folder)
+
+    # Read TXRX
+    txrx_dict = read_txrx(setup_dict)
+
+    # Read Paths (.paths)
+    path_dict_list = 0
+
+    # Read Materials (.materials)
+    materials_dict = read_materials(rt_output_folder)
+
+
+
+
+    # Read Vertices (.vertices)
+
+    # Read Faces (.faces)
+
+    # Read Objects (.objects)
+
+    # # Create scene from simulation folder
+    # scene = create_scene_from_folder(insite_sim_folder)
     
+    # # Export scene data (save {building/terrain/vegetaion}_{faces/materials}.mat files)
+    # scene_dict = scene.export_data(output_folder)
+    
+    # # Visualize if requested
+    # if vis_buildings:
+    #     scene.plot_3d(show=True)#, save=True, filename=os.path.join(output_folder, 'scene_3d.png'))
+    
+
+
+    # Save params.mat
+    # cu.save_params_dict(output_folder, c.RAYTRACER_NAME_WIRELESS_INSITE, 
+    #                    c.RAYTRACER_VERSION_WIRELESS_INSITE,
+    #                    setup_dict, txrx_dict, materials_dict, scene_dict)
+    
+    # # Save scenario to deepmimo scenarios folder
+    # scen_name = cu.save_scenario(output_folder, scen_name=scenario_name, overwrite=overwrite)
+    
+    # print(f'Zipping DeepMIMO scenario (ready to upload!): {output_folder}')
+    # cu.zip_folder(output_folder) # ready for upload
+    
+    # # Copy and zip ray tracing source files as well
+    # cu.save_rt_source_files(insite_sim_folder, SOURCE_EXTS)
+    
+    scen_name = ''
+    return scen_name
+
+
+def read_setup(load_folder: str) -> Dict:
+    return load_from_pickle(load_folder + 'sionna_rt_params.pkl')
+
+
+def read_txrx(setup_dict: Dict) -> Dict:
+    
+    # There will be two txrx sets, one for the sources and one for the targets
+
+    txrx_dict = {}
+    # Create TX and RX objects in a loop
+    for i in range(2):
+        is_tx = (i == 0)  # First iteration is TX, second is RX
+        obj = TxRxSet()
+        obj.is_tx = is_tx
+        obj.is_rx = not is_tx
+        
+        obj.name = 'tx_array' if is_tx else 'rx_array'
+        obj.id_orig = i + 1
+        obj.idx = i + 1  # 1-indexed
+        
+        # Set antenna properties        
+        obj.num_ant = 1 if setup_dict['array_synthetic'] else setup_dict[obj.name + '_num_ant']
+        obj.ant_rel_positions = setup_dict[obj.name + '_ant_pos']        
+        obj.dual_pol = setup_dict[obj.name + '_num_ant'] != setup_dict[obj.name + '_size']
+
+        txrx_dict[f'tx_rx_set_{i+1}'] = obj.to_dict() # 1-indexed
+
+    return txrx_dict
+
+
+
+def read_paths(load_folder: str, save_folder: str) -> None:
+    path_dict_list = load_from_pickle(load_folder + 'sionna_paths.pkl')
+
+    # Get TX positions (assuming constant for all paths)
+    tx_pos = path_dict_list[0]['sources']
+
+    # Get RX number and positions
+    rx_pos_all = []
+    for paths_dict in path_dict_list:
+        rx_pos_all += paths_dict['targets'].tolist()
+    rx_pos = np.unique(rx_pos_all, axis=0)
+    n_rx = rx_pos.shape[0]
+
+    # Pre-allocate matrices
+    max_iteract = min(c.MAX_INTER_PER_PATH, path_dict_list[0]['vertices'].shape[0])
+
+    data = {
+        'n_paths': np.zeros((n_rx), dtype=np.float32),
+        'rx_pos': rx_pos,
+        'tx_pos': tx_pos,
+        'aoa_az': np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'aoa_el': np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'aod_az': np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'aod_el': np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'toa':    np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'power':  np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'phase':  np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'inter':  np.zeros((n_rx, c.MAX_PATHS), dtype=np.float32) * np.nan,
+        'inter_pos': np.zeros((n_rx, c.MAX_PATHS, max_iteract, 3), dtype=np.float32) * np.nan,
+    }
+
+    # make squeeze and slice function to be applied to each Sionna array
+    ss = lambda array: array.squeeze()[..., :c.MAX_PATHS]
+
+    # Read paths 
+    last_idx = 0
+    for paths_dict in path_dict_list:
+        batch_size = paths_dict['a'].shape[1]
+        idxs = last_idx + np.arange(batch_size)
+        last_idx = idxs[-1]
+        
+        a = ss(paths_dict['a'])
+        not_nan_mask = a != 0
+        data['power'][idxs][not_nan_mask] = 20 * np.log10(np.absolute(a[not_nan_mask]))
+        data['phase'][idxs][not_nan_mask] = np.angle(a[not_nan_mask], deg=True)
+        data['toa'][idxs][not_nan_mask]   = ss(paths_dict['tau'])
+
+        data['aoa_az'][idxs][not_nan_mask] = ss(paths_dict['phi_r'])   * 180 / np.pi
+        data['aoa_el'][idxs][not_nan_mask] = ss(paths_dict['theta_r']) * 180 / np.pi
+        data['aod_az'][idxs][not_nan_mask] = ss(paths_dict['phi_t'])   * 180 / np.pi
+        data['aod_el'][idxs][not_nan_mask] = ss(paths_dict['theta_t']) * 180 / np.pi
+
+        inter_pos = paths_dict['vertices'].squeeze()[:max_iteract, :, :c.MAX_PATHS, :]
+        data['inter_pos'][idxs] = np.transpose(inter_pos, (1,2,0,3))
+
+        data['inter'][idxs][not_nan_mask] = get_sionna_interaction_types(ss(paths_dict['types']),
+                                                                         data['inter_pos'][idxs])
+                                                                          
+        
+    
+    # Compress data before saving
+    data = cu.compress_path_data(data)
+    
+    # Save each data key
+    for key in data.keys():
+        cu.save_mat(data[key], key, save_folder, 1, 1, 2)
+
+    return
+
+def get_sionna_interaction_types(types: np.ndarray, inter_pos: np.ndarray) -> np.ndarray:
+    """
+    Convert Sionna interaction types to DeepMIMO interaction codes.
+    
+    Similarities between Sionna and DeepMIMO:
+        - Sionna uses 0 for LoS. DeepMIMO too.
+
+    Important differences between Sionna and DeepMIMO:
+        - Sionna uses 1 to refer to paths with only reflections. The number of reflections in the 
+        path, however, can be 1, 2, 3, etc... The way we are able to tell which DeepMIMO code 
+        to use (respectively, 1, 11, 111, etc...) is by the size of the corresponding path vertices. 
+        - Sionna uses 2 to refer to paths with A SINGLE diffraction. It doesn't allow paths with 
+        two diffractions or paths with a diffraction and any other phenomena. So this is simple 
+        to equate in DeepMIMO, since we use the single code for diffraction, 2.
+        - Sionna uses 3 to refer to paths with a diffuse reflection / scattering event at the end. 
+        These paths end with a diffusion, but may have any number of reflections (up to max_depth-1)
+        before the diffusion. Therefore, like in the first case, we have to use the size of the vertices
+        to determine whether to use DeepMIMO code 3, 13, 113, 1113, 11113, etc. 
+        - Sionna uses 4 to refer to RIS. DeepMIMO does not support RIS yet, so we ignore this.
+        (technically, DeepMIMO supports RIS, but not yet as a path interaction type)
+
+    Args:
+        types: Array of interaction types from Sionna (N_USERS x MAX_PATHS)
+        inter_pos: Array of interaction positions (N_USERS x MAX_PATHS x MAX_INTERACTIONS x 3)
+
+    Returns:
+        np.ndarray: Array of DeepMIMO interaction codes (N_USERS x MAX_PATHS)
+    """
+    n_users, max_paths = inter_pos.shape[:2]
+    result = np.zeros((n_users, max_paths), dtype=np.float32)
+    
+    # For each path
+    for rx_idx in range(n_users):
+        for path_idx in range(max_paths):
+            
+            # Skip if no type (nan or 0)
+            if np.isnan(types[rx_idx, path_idx]) or types[rx_idx, path_idx] == 0:
+                continue
+                
+            sionna_type = int(types[rx_idx, path_idx])
+            
+            # Handle LoS case (type 0)
+            if sionna_type == 0:
+                result[rx_idx, path_idx] = c.INTERACTION_LOS
+                continue
+                
+                
+            # Count number of actual interactions by checking non-nan positions
+            n_interactions = np.nansum(~np.isnan(inter_pos[rx_idx, path_idx, :, 0]))
+            if n_interactions == 0:  # Skip if no interactions
+                continue
+                
+            # Handle different Sionna interaction types
+            if sionna_type == 1:  # Pure reflection path
+                # Create string of '1's with length = number of reflections
+                code = '1' * n_interactions
+                result[rx_idx, path_idx] = np.float32(code)
+                
+            elif sionna_type == 2:  # Single diffraction path
+                # Always just '2' since Sionna only allows single diffraction
+                result[rx_idx, path_idx] = c.INTERACTION_DIFFRACTION
+                
+            elif sionna_type == 3:  # Scattering path with possible reflections
+                # Create string of '1's for reflections + '3' at the end for scattering
+                if n_interactions > 1:
+                    code = '1' * (n_interactions - 1) + '3'
+                else:
+                    code = '3'
+                result[rx_idx, path_idx] = np.float32(code)
+                
+            else:
+                if sionna_type == 4:
+                    raise NotImplementedError('RIS code not supported yet')
+                else:
+                    raise ValueError(f'Unknown Sionna interaction type: {sionna_type}')
+    
+    return result
+
 # Unwrap materials
 def import_sionna_for_deepmimo(save_folder: str):
+
     
     saved_vars_names = [
-        'sionna_paths.pkl',
-        'sionna_materials.pkl',
-        'sionna_material_indices.pkl',
-        'sionna_rt_params.pkl',
+        'sionna_paths.pkl', # DONE
+        'sionna_materials.pkl',        # NOW
+        'sionna_material_indices.pkl', # NOW
+        'sionna_rt_params.pkl', # DONE
         'sionna_vertices.pkl',
         'sionna_faces.pkl',
         'sionna_objects.pkl',
@@ -34,7 +307,11 @@ def import_sionna_for_deepmimo(save_folder: str):
 
     return
 
-def load_materials(load_folder):
+
+
+
+
+def read_materials(load_folder):
 
     material_properties = 0
 
@@ -57,62 +334,17 @@ def load_materials(load_folder):
         scattering_model = 'none' if not scat_coeff else scattering_model
 
 
-def load_paths(load_folder):
-    path_dict_list = load_from_pickle(load_folder + 'sionna_paths.pkl')
-
-
-    tx_pos = path_dict_list[0]['sources']
-
-    rx_pos_all = []
-    for obj_idx, paths_dict in enumerate(path_dict_list):
-        rx_pos_all += paths_dict['targets'].tolist()
-    rx_pos = np.unique(rx_pos_all, axis=0)
-    n_rx = rx_pos.shape[0]
-
-    # Pre-allocate matrices
-    MAX_PATHS = 25
-    MAX_INTER_PER_PATH = 10
-
-    max_iteract = min(MAX_INTER_PER_PATH, path_dict_list[0]['vertices'].shape[0])
-
-    data = {
-        'n_paths': np.zeros((n_rx), dtype=np.float32),
-        'aoa_az': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'aoa_el': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'aod_az': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'aod_el': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'toa': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'power': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'phase': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'inter': np.zeros((n_rx, MAX_PATHS), dtype=np.float32) * np.nan,
-        'inter_pos': np.zeros((n_rx, MAX_PATHS, max_iteract, 3), dtype=np.float32) * np.nan,
-    }
-
-    # squeeze and slice function to be applied to each array
-    ss = lambda array: array.squeeze()[..., :MAX_PATHS]
-
-    last_idx = 0
-    for obj_idx, paths_dict in enumerate(path_dict_list):
-
-        batch_size = paths_dict['a'].shape[1]
-        idxs = last_idx + np.arange(batch_size)
-        last_idx = idxs[-1]
-        
-        a = ss(paths_dict['a'])
-        not_nan_mask = a != 0
-        data['power'][idxs][not_nan_mask] = 20 * np.log10(np.absolute(a[not_nan_mask]))
-        data['phase'][idxs] = np.angle(a, deg=True)
-        data['toa'][idxs]   = ss(paths_dict['tau'])
-        data['aoa_az'][idxs] = ss(paths_dict['phi_r'])   * 180 / np.pi
-        data['aoa_el'][idxs] = ss(paths_dict['theta_r']) * 180 / np.pi
-        data['aod_az'][idxs] = ss(paths_dict['phi_t'])   * 180 / np.pi
-        data['aod_el'][idxs] = ss(paths_dict['theta_t']) * 180 / np.pi
-        data['inter'][idxs]     = ss(paths_dict['types'])
-        
-        inter_pos = paths_dict['vertices'].squeeze()[:max_iteract, :, :MAX_PATHS, :]
-        data['inter_pos'][idxs] = np.transpose(inter_pos, (1,2,0,3))
 
 
 def load_vertices(load_folder):
     pass
 
+
+
+
+
+
+if __name__ == '__main__':
+    setup_dict = read_setup('...')
+    txrx_dict = read_txrx(setup_dict)
+    
