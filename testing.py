f3d50e22eded72feb4dc40cf9b737b108b87f8b9
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-03-25 01:39:37 -0700

    setup testing usecase for channels in nested dicts

--- testing.py
+++ testing.py
@@ -5,19 +5,18 @@ import numpy as np
 import deepmimo as dm
 import matplotlib.pyplot as plt
 
-
 from my_api_key import API_KEY as MY_API_KEY
 
 #%% V4 Conversion
 
 # Example usage
-# rt_folder = './P2Ms/asu_campus'
+rt_folder = './P2Ms/asu_campus'
 # rt_folder = './P2Ms/simple_street_canyon_test'
 # rt_folder = 'C:/Users/jmora/Documents/GitHub/AutoRayTracing/all_runs/run_02-02-2025_15H45M26S/scen_0/DeepMIMO_folder'
 # rt_folder = 'C:/Users/jmora/Documents/GitHub/AutoRayTracing/all_runs/run_02-02-2025_15H45M26S/scen_0/sionna_test'
 # rt_folder = 'C:/Users/jmora/Documents/GitHub/AutoRayTracing/all_runs/run_02-02-2025_15H45M26S/scen_0/sionna_export_test2'
 # rt_folder = r'C:\Users\jmora\Downloads\DeepMIMOv4-hao-test\all_runs\run_03-08-2025_15H38M57S\NewYork\sionna_export_full'
-rt_folder = r'C:\Users\jmora\Documents\GitHub\AutoRayTracing\all_runs\run_03-09-2025_18H18M51S\NewYork\sionna_export_RX'
+# rt_folder = r'C:\Users\jmora\Documents\GitHub\AutoRayTracing\all_runs\run_03-09-2025_18H18M51S\NewYork\sionna_export_RX'
 
 scen_name = os.path.basename(rt_folder)
 dm.convert(rt_folder, overwrite=True, scenario_name=scen_name, vis_scene=True)
@@ -40,6 +39,81 @@ o  = load_pickle(os.path.join(rt_folder, 'sionna_objects.pkl'))
 
 #%%
 
+scen_name = 'asu_campus_3p5'
+dataset = dm.load(scen_name)[0]
+# dataset = dm.load(model, matrices=relevant_mats)[0]
+
+ch_params = dm.ChannelGenParameters()
+ch_params.bs_antenna.shape = np.array([10, 1])
+
+# Reduce dataset size with uniform sampling 
+uni_idxs = dataset.get_uniform_idxs([3,3])
+dataset_u = dataset.subset(uni_idxs)
+
+# Consider only active users for redundancy reduction
+dataset_t = dataset_u.subset(dataset_u.get_active_idxs())  # ...
+
+print(f'dataset.n_ue = {dataset.n_ue}')
+print(f'dataset_u.n_ue = {dataset_u.n_ue}')
+print(f'dataset_t.n_ue = {dataset_t.n_ue}')
+
+
+#%%
+
+dataset = dm.load(scen_name)[0]
+
+# Create channel parameters with all options
+ch_params = dm.ChannelGenParameters()
+
+# Antenna parameters
+
+# Base station antenna parameters
+ch_params.bs_antenna.rotation = np.array([0, 0, 0])  # [az, el, pol] in degrees
+ch_params.bs_antenna.shape = np.array([8, 1])        # [horizontal, vertical] elements
+ch_params.bs_antenna.spacing = 0.5                   # Element spacing in wavelengths
+
+# User equipment antenna parameters
+ch_params.ue_antenna.rotation = np.array([0, 0, 0])  # [az, el, pol] in degrees
+ch_params.ue_antenna.shape = np.array([1, 1])        # [horizontal, vertical] elements
+ch_params.ue_antenna.spacing = 0.5                   # Element spacing in wavelengths
+
+# Channel parameters
+ch_params.freq_domain = True  # Whether to compute frequency domain channels
+ch_params.num_paths = 10      # Number of paths
+
+# OFDM parameters
+ch_params.ofdm.bandwidth = 10e6                      # Bandwidth in Hz
+ch_params.ofdm.subcarriers = 512                     # Number of subcarriers
+ch_params.ofdm.selected_subcarriers = np.arange(1)   # Which subcarriers to generate
+ch_params.ofdm.rx_filter = 0                         # Receive Low Pass / ADC Filter
+
+ch_params.ofdm.aaa = 0
+dataset.set_channel_params(ch_params)
+
+# Generate channels
+# dataset.compute_channels(ch_params)
+dataset.channel.shape
+
+
+#%% [!] Load matrices
+
+import os
+import numpy as np
+
+rt_scens = ['asu_campus_3p5', 'city_0_newyork_3p5', ]
+
+ch_params = dm.ChannelGenParameters()
+ch_params.ofdm.selected_subcarriers = np.arange(10 * 12)
+ch_params.bs_antenna.shape = np.array([10, 1])
+ch_params.bs_antenna.rotation = np.array([0, 0, -135])
+
+data_matrices = {}
+
+data_folder = 'stochastic_data'
+models = rt_scens
+for model in models:
+    dataset = dm.load(model)[0]
+    data_matrices[model] = dataset.compute_channels(ch_params)
 
 
 #%%
@@ -143,14 +217,11 @@ for subfolder in subfolders[:-5]:
 
     # dm.load(scen_name, 'matrices': None)
 
-    # TODO: Make this work for loading only the first tx and rx set
     try:
-        d = dm.load(scen_name, tx_sets={1: []}, rx_sets={2: []})
+        d = dm.load(scen_name, tx_sets={1: []}, rx_sets={2: []}, matrices=None)
     except Exception as e:
-        d = dm.load(scen_name, tx_sets={1: []}, rx_sets={4: []})
+        d = dm.load(scen_name, tx_sets={1: []}, rx_sets={4: []}, matrices=None)
     _, ax = d.scene.plot(show=False)
     ax.set_title(scen_name + ': ' + ax.get_title())
     plt.show()
 
-
-# TODO: Give argument to also not load any matrices (only scene)
