742b9a2c6d783c232fd5f0a5ed65084d092354a7
J.A.Morais <19170303+jmoraispk@users.noreply.github.com>
2025-04-27 20:13:29 -0700

    add user grid to scenario image

--- scripts/dm_converter_loop.py
+++ scripts/dm_converter_loop.py
@@ -6,7 +6,7 @@ import os
 import json
 import shutil
 import matplotlib.pyplot as plt
-from api_keys import API_KEY as MY_API_KEY
+from api_keys import DEEPMIMO_API_KEY
 
 # Main execution
 EXECUTION_MODE = 'collect_errors' # 'collect_errors' | 'retry_errors'
@@ -28,19 +28,19 @@ if EXECUTION_MODE == 'retry_errors' and os.path.exists(ERROR_LOG_FILE):
 timing_results = {}
 
 # For conversion
-for subfolder in subfolders:
-    scen_name = os.path.basename(subfolder)
-    print(f'running: {subfolder}')
+# for subfolder in subfolders:
+#     scen_name = os.path.basename(subfolder)
+#     print(f'running: {subfolder}')
 
 # For zipping
-# for scen_name in dm.get_available_scenarios():
+for scen_name in dm.get_available_scenarios():
 
     # if 'asu' in scen_name:
     #     continue
 
-    if not scen_name.startswith('city_'):
+    if not scen_name.startswith('city_') or not scen_name.endswith('_s'):
         continue
-    
+
     start = time.time()
     try:
         # scen_name = dm.convert(subfolder, overwrite=True, scenario_name=scen_name, 
@@ -48,15 +48,26 @@ for subfolder in subfolders:
         # print(f"Conversion successful: {scen_name}")
         stop = time.time()
 
-        # dm.upload(scen_name, key=MY_API_KEY)
+        # dm.upload(scen_name, key=DEEPMIMO_API_KEY)
         # print(f"Upload successful: {scen_name}")
-        img_name = os.path.basename(subfolder) + '_processed_zoomed.png'
-        img_path = os.path.join(subfolder, 'figs', img_name)
-
-        # print(os.path.exists(img_path))
+        
         full_dataset = dm.load(scen_name)
         dataset = dm.load(scen_name)[4]
-        _, ax = dataset.scene.plot(title=False)
+        
+        grid_size = dataset.grid_size - 1
+        import numpy as np
+        cols = np.arange(grid_size[0], step=6)
+        rows = np.arange(grid_size[1], step=6)
+        idxs = np.array([j + i*grid_size[0] for i in rows for j in cols])
+        # idxs = dataset.get_uniform_idxs([4,4])
+        
+        fig, ax = plt.subplots(figsize=(15, 15), subplot_kw={'projection': '3d'})
+        ax.scatter(xs=dataset.rx_pos[idxs,0], 
+                   ys=dataset.rx_pos[idxs,1], 
+                   zs=dataset.rx_pos[idxs,2]-3, 
+                   s=20, color='red', label='Users', marker='o', alpha=0.2)
+        
+        ax = full_dataset.scene.plot(title=False, ax=ax)
         ax.view_init(elev=90, azim=-90)
         ax.scatter(xs=full_dataset[0].bs_pos[0,0], 
                           ys=full_dataset[0].bs_pos[0,1], 
@@ -70,20 +81,23 @@ for subfolder in subfolders:
                           ys=full_dataset[2].bs_pos[0,1], 
                           zs=full_dataset[2].bs_pos[0,2], 
                           s=250, color='green', label='BS 3', marker='*')
-        ax.legend(ncol=3, loc='center',bbox_to_anchor=(0.5, 0.85), fontsize=20).set_zorder(1e9)
-        img_path = f'{scen_name}_scene.png'
+
+        ax.legend(ncol=4, loc='center', bbox_to_anchor=(0.5, 0.85), fontsize=20).set_zorder(1e9)
+        plt.show()
+        
         # ax.set_position([-0.1, -0.1, 1.2, 1.2])
-        plt.savefig(img_path, dpi=150, bbox_inches='tight')#, pad_inches=0)
-        plt.close()
+        # img_path = f'{scen_name}_scene.png'
+        # plt.savefig(img_path, dpi=150, bbox_inches='tight')#, pad_inches=0)
+        # plt.close()
         
         # call PIL to crop image
-        from PIL import Image
-        img = Image.open(img_path)
-        img = img.crop((150, 150, 1550, 1550))
-        img.save(img_path)
-
-        dm.upload_images(scen_name, key=MY_API_KEY, img_paths=[img_path])
-        print(f"Upload successful: {scen_name}")
+        # from PIL import Image
+        # img = Image.open(img_path)
+        # img = img.crop((150, 150, 1550, 1550))
+        # img.save(img_path)
+        break
+        # dm.upload_images(scen_name, key=DEEPMIMO_API_KEY, img_paths=[img_path])
+        # print(f"Upload successful: {scen_name}")
         if os.path.exists(img_path):
             os.remove(img_path)
             print(f"Deleted image: {img_path}")
@@ -99,6 +113,7 @@ for subfolder in subfolders:
     
     timing_results[scen_name] = stop - start
 
+#%%
 if timing_results:
     print("\nPer-scenario timing:")
     print("-" * 50)
